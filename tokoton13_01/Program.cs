d35135d8eb58d8ba8c50447588f922e28764e1fa
kobayashi tomoaki <t.koba0330@hotmail.co.jp>
2016-12-20 00:35:24 +0900

    とことんC# 13章 開始

---
+++ tokoton13_01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodai
+{
+    class X {  internal static void SayMyName() { Console.WriteLine("I am Kodai."); } }
+}
+
+namespace Shima
+{
+    class X {  internal static void SayMyName() { Console.WriteLine("I am Shima."); } }
+}
+
+
+
+namespace tokoton13_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kodai.X.SayMyName();
+            Shima.X.SayMyName();
+
+            Console.ReadLine();
+        }
+    }
+}
